2a72dcbdc540e88532a2830116c74f47c82873d4
Jordan Jefferson <jjeffe10@gmail.com>
2018-05-22 11:17:31 -0500

    Generated Getters and Setters for Car.java parameters.

--- app/src/main/java/jordan_jefferson/com/gasbudgeter/Car.java
+++ app/src/main/java/jordan_jefferson/com/gasbudgeter/Car.java
@@ -26,4 +26,44 @@ public class Car {
         return mpg * tankCapacity_gallons;
     }
 
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMpg() {
+        return mpg;
+    }
+
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
+
+    public int getTankCapacity_gallons() {
+        return tankCapacity_gallons;
+    }
+
+    public void setTankCapacity_gallons(int tankCapacity_gallons) {
+        this.tankCapacity_gallons = tankCapacity_gallons;
+    }
+
 }
